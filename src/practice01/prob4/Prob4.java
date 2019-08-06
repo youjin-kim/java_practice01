83c83ef8f21968b9bab163bd2b181cf2be34868a
Xnote <Xnote@LG>
2019-08-06 17:38:25 +0900

    4번 완료

--- src/practice01/prob4/Prob4.java
+++ src/practice01/prob4/Prob4.java
@@ -1,6 +1,26 @@
 package practice01.prob4;
 
+import java.util.Scanner;
+
 public class Prob4 {
 	public static void main (String args[]) {
+		Scanner scanner = new Scanner( System.in );
+		
+		System.out.print("문자열을 입력하세요: ");
+		String str = scanner.nextLine();
+		
+		char[] arrayString = new char[str.length()];
+		
+		int j = 0;
+		
+		while(j < arrayString.length) {
+			for(int i=0; i<=j; i++) {
+				arrayString[i] = (str.charAt(i));
+				System.out.print(arrayString[i]);
+			}
+			j++;
+			System.out.println();
+		}
+		
 	}
 }
