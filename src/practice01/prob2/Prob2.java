a3db827a5bac217f1051a0aeedfafdf1b94cf1c0
Xnote <Xnote@LG>
2019-08-06 17:10:51 +0900

    2번 완료

--- src/practice01/prob2/Prob2.java
+++ src/practice01/prob2/Prob2.java
@@ -2,5 +2,16 @@ package practice01.prob2;
 
 public class Prob2 {
 	public static void main (String args[]) {
+		int j = 11;
+		int i = 1;
+		while(i < 10) {
+			for(int k = i; k<j; k++)
+			{
+				System.out.print(k + " ");
+			}
+			i++;
+			j++;
+			System.out.println();
+		}
 	}
 }
