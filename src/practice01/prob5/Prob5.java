9a481a13710afcfcdeb670d9403627f972f9c30d
Xnote <Xnote@LG>
2019-08-06 21:14:47 +0900

    5번 완료

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -2,5 +2,25 @@ package practice01.prob5;
 
 public class Prob5 {
 	public static void main (String args[]) {
+		for(int i=1; i<100; i++) {
+			if(i<10) {
+				if(i%3==0) {
+					System.out.println(i + " 짝");
+				}
+			}
+			else if(i>10) {
+				int j=i/10;
+				int k=i%10;
+				if(j%3==0 && k%3==0) {
+					System.out.println(i + " 짝짝");
+				}
+				if(k%3!=0 && j%3==0) {
+					System.out.println(i + " 짝");
+				}
+				if(j%3!=0 && k!=0 && k%3==0) {
+					System.out.println(i + " 짝");
+				}
+			}
+		}
 	}
 }
