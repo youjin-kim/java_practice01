8a242ac383b755069466edcf03e7e2210ce7bb58
venture21 <park0122@gmail.com>
2019-08-06 16:02:30 +0900

    1번 완료

--- src/practice01/prob1/Prob1.java
+++ src/practice01/prob1/Prob1.java
@@ -1,6 +1,17 @@
 package practice01.prob1;
 
+import java.util.Scanner;
+
 public class Prob1 {
 	public static void main (String args[]) {
+		Scanner scanner = new Scanner( System.in );
+	
+		System.out.print("수를 입력하세요 : ");
+		int number  = scanner.nextInt();
+		
+		System.out.println(number);
+		
+		
+		scanner.close();
 	}
 }
